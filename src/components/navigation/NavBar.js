7fc5f93f6bcc3b18a45b1c4d32e81d2bef1d554b
Dima <dimazayac@gmail.com>
2020-12-26 12:15:41 +0100

    primirive navigation

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -2,15 +2,38 @@ import React from 'react';
 
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import VapaLogo from '../../img/vapa_logo.png'
 
 
 function NavBar() {
     return(
         <Navbar style={{ display: 'flex', justifyContent: 'space-around' }}>
-            <Navbar.Brand href="#home">VAPA</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
+            <Navbar.Brand href="#home">
+                <img style={{ width: '30px' }}
+                src={VapaLogo} 
+                alt={'vapa_logo'} />
+            </Navbar.Brand>
+                <Nav >
+                    <Nav.Link
+                    style={{ margin: '10px' }} 
+                    href="#link"
+                    >Про Vapa</Nav.Link>
+
+                    <Nav.Link
+                    style={{ margin: '10px' }} 
+                    href="#link"
+                    >Контакты</Nav.Link>
+
+                    <Nav.Link
+                    style={{ margin: '10px' }} 
+                    href="#link"
+                    >Link</Nav.Link>
+
+                    <Nav.Link
+                    style={{ margin: '10px' }} 
+                    href="#link"
+                    >Link</Nav.Link>
+                    
                 </Nav>
         </Navbar>
 )
