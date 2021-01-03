72a598f022adf343576eceded1894488afd4cdab
Dima <dimazayac@gmail.com>
2021-01-03 16:06:06 +0100

    added modal

--- src/components/products/ProductCard.js
+++ src/components/products/ProductCard.js
@@ -3,7 +3,8 @@ import '../products/ProductCard.css';
 
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import img123 from '../../img/img123.jpg'
+import img123 from '../../img/img123.jpg';
+import Modal from 'react-bootstrap/Modal';
 
 const DATABASE_URL = 'https://dima-leveret-vapa-default-rtdb.firebaseio.com'
 
@@ -11,6 +12,7 @@ class ProductCard extends React.Component {
 
     state = {
         productList: [],
+        show: false,
     }
 
     fetchProducts = () => {
@@ -42,22 +44,47 @@ class ProductCard extends React.Component {
         this.fetchProducts()
     }
 
+    openModal = () => {
+        this.setState({
+            show: true,
+        })
+        console.log('modal');
+    }
+
     render (){
         return (
             <div className="bobyContainer" >
                 <div className='cardContainer' >
                 {
                     this.state.productList.map(product => (
-                    <Card className='card' key={product.id} >
+                    <Card className='card'  key={product.id} >
                             <Card.Img variant="top" src={img123} alt='some picture' className='cardImg'/>
                         <Card.Body>
                             <Card.Title> {product.product} "{product.name}" </Card.Title>
                             <Card.Text> {product.description} </Card.Text>
-                            
                         </Card.Body>
-                        <Button className="cardButton" > В кошик </Button>
+                        <div>
+                            <Button 
+                            onClick={this.openModal}
+                            className="cardButton" > Детальніше </Button>
+                            <Button className="cardButton" > В кошик </Button>
+                        </div>
+                        <Modal show={this.state.show} onHide={this.state.show}>
+                            <Modal.Header closeButton>
+                            <Modal.Title>Modal heading</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                            <Modal.Footer>
+                            <Button variant="secondary" onClick={this.state.show}>
+                                Close
+                            </Button>
+                            <Button variant="primary" onClick={this.state.show}>
+                                Save Changes
+                            </Button>
+                            </Modal.Footer>
+                        </Modal>
                     </Card>
-                    ))
+                ))
                 }
                 </div>
             </div>
