46325bdf3ee7caa088960a81704ecd77001dc417
Dima <dimazayac@gmail.com>
2021-02-07 16:48:08 +0100

    added form to add new product

--- src/components/products/AddProduct.js
+++ src/components/products/AddProduct.js
@@ -3,6 +3,7 @@ import  './AddProduct.css';
 
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 class AddProduct extends React.Component {
 
@@ -31,26 +32,98 @@ class AddProduct extends React.Component {
                className="addButton"
                onClick={() => this.openModal()} 
                > 
-                Додай продукт 
+                Add new product 
                </Button>
 
                <Modal
                         
-                        size="lg"
-                        aria-labelledby="contained-modal-title-vcenter"
-                        centered
-                        show={this.state.open}
-                        onHide={this.closeModal} 
-                        >
-                        <Modal.Header closeButton>
-                        <Modal.Title> Додай продукт Миха! </Modal.Title>
-                        </Modal.Header>
+                    size="lg"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    centered
+                    show={this.state.open}
+                    onHide={this.closeModal} 
+                    >
+                    <Modal.Header closeButton>
+                    <Modal.Title> Add new product </Modal.Title>
+                    </Modal.Header>
                         
-                        <Modal.Body>
-                            Фрмуляж продукции
-                        </Modal.Body>
+                    <Modal.Body>
 
-                    </Modal>
+                        <Form>
+                            <Form.Group controlId="exampleForm.ControlInput1">
+                                <Form.Label>Product name</Form.Label>
+                                <Form.Control type="text" />
+
+                                <Form.Label>Product</Form.Label>
+                                <Form.Control type="text" />
+
+                                <Form.Label>Description</Form.Label>
+                                <Form.Control type="text" />
+
+                                <Form.Label>Full description</Form.Label>
+                                <Form.Control as="textarea" rows={3} />
+
+                                <Form.Label>Details</Form.Label>
+                                <Form.Control as="textarea" rows={3} />
+
+                                <Form.Label>Usage</Form.Label>
+                                <Form.Control as="textarea" rows={3} />
+
+                                <Form.Label>Structure</Form.Label>
+                                <Form.Control as="textarea" rows={3} />
+
+                                <Form.Label>Preservation</Form.Label>
+                                <Form.Control as="textarea" rows={3} />
+
+                                <Form.Label>Date</Form.Label>
+                                <Form.Control type="text" />
+
+                                <Form.Label>Volume</Form.Label>
+                                <Form.Control type="number" />
+
+                                <Form.Label>Amount</Form.Label>
+                                <Form.Control type="number" />
+
+                            </Form.Group>
+                            <Form.Group controlId="exampleForm.ControlSelect1">
+                                <Form.Label>Select product type</Form.Label>
+                                <Form.Control as="select">
+                                <option>hair</option>
+                                <option>face</option>
+                                <option>body</option>
+                                <option>accessories</option>
+                                </Form.Control>
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>Sails or Complex</Form.Label>
+                            <Form.Check 
+                                type='checkbox'
+                                label='sails'
+                            />
+                            <Form.Check 
+                                type='checkbox'
+                                label='complex'
+                            />
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>Price</Form.Label>
+                                <Form.Control type="number" />
+
+                                <Form.Label>New price</Form.Label>
+                                <Form.Control type="number" />
+                            </Form.Group>
+                            
+                        </Form>
+                    </Modal.Body>
+
+                    <Button 
+                        variant="outline-success" 
+                        className="addButton" 
+                    > 
+                        Add  
+                    </Button>
+
+                </Modal>
 
             </>
         )
