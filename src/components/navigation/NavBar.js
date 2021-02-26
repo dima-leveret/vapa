b3238a4134792c9dcc4b18a706694089daecf667
Dima <dimazayac@gmail.com>
2021-02-26 16:57:44 +0100

    added signIn/signOut

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
+import firebase from 'firebase';
 
 import '../navigation/NavBar.css';
 
 import { FiInstagram } from 'react-icons/fi';
+
+import Button from 'react-bootstrap/Button'
 
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
@@ -22,75 +25,105 @@ import Home from '../home/Home';
 
 
 
- function NavBar() {
+ class NavBar extends React.Component {
 
-    return(
-        <>
-            <Navbar className='navBar'>
-                <Link to='/home' >
-                    <Navbar.Brand 
-                    className='navBrand'
-                     >
-                        <Image 
-                        className="navBrandImage" 
-                        src={WhiteVapaLogo} alt={'vapa_logo'} />
-                    </Navbar.Brand>
-                </Link> 
-                <Nav  className='nav' >
-                    <Link  to='/aboutVapa' className='navLink' >
-                        About Vapa
-                    </Link>
-                            
-                    <Link to='/contacts' className='navLink' >
-                        Contacts
-                    </Link>
-        
-                    <Link to='/partners' className='navLink' >
-                        Partners
-                    </Link>
-        
-                    <Link to='/paymentAndDelivery' className='navLink' >
-                        Payment and delivery
-                    </Link>
-                </Nav>
-                <ShoppingCard/>
-                <Link to='/sign-in'  className='navLink'>
-                    Sign in
-                </Link>
-                <a href='https://www.instagram.com/vapa.eco/' target='blank' >
-                    <FiInstagram className='instaIcon' />
-                </a>
-            </Navbar>
-        
-            <Switch>
-                <Route exact path='/home' >
-                    <Home/>
-                </Route>
-        
-                <Route path='/aboutVapa' >
-                    <AboutVapa />
-                </Route>
-        
-                <Route path='/contacts' >
-                    <Contacts />
-                </Route>
-        
-                <Route path='/partners' >
-                    <Partners />
-                </Route>
-        
-                <Route path='/paymentAndDelivery' >
-                    <PaymentAndDelivery />
-                </Route>
+    state = {
+        user: null,
+    }
 
-                <Route path='/sign-in' >
-                    <Sign/>
-                </Route>
-        
-            </Switch>
-        </>
-    )
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            this.setState({
+                user
+            })
+        })
+    }
 
+    signOut = () => {
+        firebase.auth().signOut()
+    }
+
+    render () {
+
+        return(
+            <>
+                <Navbar className='navBar'>
+                    <Link to='/home' >
+                        <Navbar.Brand 
+                        className='navBrand'
+                         >
+                            <Image 
+                            className="navBrandImage" 
+                            src={WhiteVapaLogo} alt={'vapa_logo'} />
+                        </Navbar.Brand>
+                    </Link> 
+                    <Nav  className='nav' >
+                        <Link  to='/aboutVapa' className='navLink' >
+                            About Vapa
+                        </Link>
+                                
+                        <Link to='/contacts' className='navLink' >
+                            Contacts
+                        </Link>
+            
+                        <Link to='/partners' className='navLink' >
+                            Partners
+                        </Link>
+            
+                        <Link to='/paymentAndDelivery' className='navLink' >
+                            Payment and delivery
+                        </Link>
+                    </Nav>
+                    <ShoppingCard/>
+                    
+
+                    {this.state.user
+                    ?
+                    <Button
+                        onClick={this.signOut} 
+                        variant='warning' 
+                    >Sign out</Button>
+                    : 
+                    <Link to='/sign-in'  className='navLink'>
+                        <Button variant='success' >Sign in</Button>
+                    </Link>
+                    }
+
+                    <a href='https://www.instagram.com/vapa.eco/' target='blank' >
+                        <FiInstagram className='instaIcon' />
+                    </a>
+                </Navbar>
+            
+                <Switch>
+                    <Route exact path='/home' >
+                        <Home/>
+                    </Route>
+            
+                    <Route path='/aboutVapa' >
+                        <AboutVapa />
+                    </Route>
+            
+                    <Route path='/contacts' >
+                        <Contacts />
+                    </Route>
+            
+                    <Route path='/partners' >
+                        <Partners />
+                    </Route>
+            
+                    <Route path='/paymentAndDelivery' >
+                        <PaymentAndDelivery />
+                    </Route>
+    
+                    <Route path='/sign-in' >
+                        <Sign/>
+                    </Route>
+            
+                </Switch>
+            </>
+        )
+
+    }
     
 }
 
